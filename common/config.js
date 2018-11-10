da00c63c82567c020d5705bb6451647faf4c4bf9
obrad <obrad.nikovic@gmail.com>
2018-11-10 22:03:09 +0100

    config.js

--- common/config.js
+++ common/config.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
-Promise = require('bluebird');
-_ = require('lodash');
 const os = require('os');
-
-var winston = require('winston');
+const winston = require('winston');
+const Slack = require('node-slack')
 require('winston-loggly-bulk');
 
 const transports = [
-  new (winston.transports.Console)({
-    timestamp: true,
-    colorize: true
-  })
+    new winston.transports.Console({
+        timestamp: true,
+        colorize: true,
+        format: winston.format.simple()
+    })
 ]
 
-if(process.env.ENV=='production'){
+if(process.env.ENV === 'production'){
   transports.push(
-    new (winston.transports.Loggly)({
+    new winston.transports.Loggly({
       token: process.env.LOGGLY_KEY,
       subdomain: "pricefy",
       json: true,
@@ -24,8 +23,18 @@ if(process.env.ENV=='production'){
   )
 }
 
-logger = new (winston.Logger)({
-  handleExceptions: true,
-  level: process.env.LOGGER_LEVEL,
-  transports: transports
-});
+global.loger = winston.createLogger({
+    handleExceptions: true,
+    level: process.env.LOGGER_LEVEL,
+    transports: transports
+})
+
+global.AlertSvc = message => {
+    const slack = new Slack(process.env.SLACK_HOOK);
+    const compositeMessage = '['+os.hostname()+']['+process.env.ENV+'] '+message;
+    slack.send({
+        text: compositeMessage,
+        channel: '#motors-alerts',
+        username: 'MotorsBot'
+    });
+}
