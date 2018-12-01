831fd3282ebf238a56c7339b5cf84c83ab4c2132
obrad <obrad.nikovic@gmail.com>
2018-12-02 00:02:53 +0100

    geo

--- Utils/GoogleGeoApi/GoogleGeoApi.js
+++ Utils/GoogleGeoApi/GoogleGeoApi.js
@@ -2,24 +2,30 @@ const googleMapsClient = require ('@google/maps');
 class GoogleGeoApi{
 
     constructor(){
-        this.api_key = process.env.GEO_API_KEY;
-    }
 
+        this.api_key =process.env.GEO_API_KEY;
+
+    }
     GeoApiCall(address){
-        return new Promise((resolve, reject) => {
-            let client = googleMapsClient.createClient({key: this.api_key});
-            client.geocode({ address }, (err, response) => {
+
+        return new Promise((resolve,reject)=>{
+            let client = googleMapsClient.createClient({key:this.api_key});
+            client.geocode({
+                address: address
+            }, function(err, response) {
                 if (!err) {
-                    console.log(response.json.results);
                     resolve(response.json.results);
                 }else {
                     console.log(err);
                     reject(err);
                 }
             });
+
+
         })
+
     }
+
 }
 
-
-module.exports = GoogleGeoApi;
+module.exports=GoogleGeoApi;
